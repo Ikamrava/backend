715235ea1ad7d8e16db11d3f3a0681908124a7c1
ikamrava <kamrava.iman@outlook.com>
2023-04-28 22:53:41 +0100

    SECRET_KEY

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get["SECRET_KEY", "515651513131365153"]
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
