ba9ad225e144863ee4af0d31fa6a59118ae21e75
ikamrava <kamrava.iman@outlook.com>
2023-04-28 20:05:51 +0100

    first commit

---
+++ base/products.py
@@ -0,0 +1,99 @@
+products = [
+    {
+        "id": "1",
+        "name": "Aiarpod",
+        "image": "https://i.ebayimg.com/images/g/2UsAAOSwXGhjWnok/s-l640.png",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;",
+        "brand": "Apple",
+        "category": "Electronics",
+        "price": 25.25,
+        "countinstock": 10,
+        "rating": 4.5,
+        "numReviews": 20
+    },
+    {
+        "id": "2",
+        "name": "Iphone",
+        "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-12-white-2020?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1635202752000",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;wdwqdqwdqwdqwdwqdqwdqwdqwdqwdqwdqwdqwdqwdwqdqwdqd",
+        "brand": "Apple",
+        "category": "Cell Phone",
+        "price": 1025.25,
+        "countinstock": 10,
+        "rating": 3.5,
+        "numReviews": 20
+    },
+    {
+        "id": "3",
+        "name": "Aiarpod",
+        "image": "https://i.ebayimg.com/images/g/2UsAAOSwXGhjWnok/s-l640.png",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;",
+        "brand": "Apple",
+        "category": "Electronics",
+        "price": 25.25,
+        "countinstock": 10,
+        "rating": 4.5,
+        "numReviews": 20
+    },
+    {
+        "id": "4",
+        "name": "Iphone",
+        "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-12-white-2020?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1635202752000",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;wdwqdqwdqwdqwdwqdqwdqwdqwdqwdqwdqwdqwdqwdwqdqwdqd",
+        "brand": "Apple",
+        "category": "Cell Phone",
+        "price": 1025.25,
+        "countinstock": 10,
+        "rating": 3.5,
+        "numReviews": 20
+    },
+    {
+        "id": "5",
+        "name": "Aiarpod",
+        "image": "https://i.ebayimg.com/images/g/2UsAAOSwXGhjWnok/s-l640.png",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;",
+        "brand": "Apple",
+        "category": "Electronics",
+        "price": 25.25,
+        "countinstock": 10,
+        "rating": 4.5,
+        "numReviews": 20
+    },
+    {
+        "id": "6",
+        "name": "Iphone",
+        "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-12-white-2020?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1635202752000",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;wdwqdqwdqwdqwdwqdqwdqwdqwdqwdqwdqwdqwdqwdwqdqwdqd",
+        "brand": "Apple",
+        "category": "Cell Phone",
+        "price": 1025.25,
+        "countinstock": 10,
+        "rating": 3.5,
+        "numReviews": 20
+    },
+    {
+        "id": "7",
+        "name": "Aiarpod",
+        "image": "https://i.ebayimg.com/images/g/2UsAAOSwXGhjWnok/s-l640.png",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;",
+        "brand": "Apple",
+        "category": "Electronics",
+        "price": 25.25,
+        "countinstock": 10,
+        "rating": 4.5,
+        "numReviews": 20
+    },
+    {
+        "id": "8",
+        "name": "Iphone",
+        "image": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-12-white-2020?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1635202752000",
+        "description": "wflmwf;lmw;fm;wqmfe#;qwmf#;mqwf;wdwqdqwdqwdqwdwqdqwdqwdqwdqwdqwdqwdqwdqwdwqdqwdqd",
+        "brand": "Apple",
+        "category": "Cell Phone",
+        "price": 1025.25,
+        "countinstock": 10,
+        "rating": 3.5,
+        "numReviews": 20
+    },
+
+]
